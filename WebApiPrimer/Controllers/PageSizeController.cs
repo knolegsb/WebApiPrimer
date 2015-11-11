fdfac0638ef85fd16ff3e2d73afd7eae533bffb9
knolegsb <seon2you@hotmail.com>
2015-11-10 22:40:41 -0800

    added api controller

---
+++ WebApiPrimer/Controllers/PageSizeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+
+namespace WebApiPrimer.Controllers
+{
+    public class PageSizeController : ApiController
+    {
+        private static string TargetUrl = "http://apress.com";
+
+        public long GetPageSize()
+        {
+            WebClient wc = new WebClient();
+            Stopwatch sw = Stopwatch.StartNew();
+            byte[] apressData = wc.DownloadData(TargetUrl);
+            Debug.WriteLine("Elapsed ms: {0}", sw.ElapsedMilliseconds);
+            return apressData.LongLength;
+        }
+    }
+}
